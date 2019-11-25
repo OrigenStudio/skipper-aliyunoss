ed96f3f798f2585ef5b4e79d1dc2de60c0246da5
Oscar Chic Farré <oscarchicfarre@gmail.com>
2019-11-25 13:22:57 +0100

    Improvements

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ var co = require("co");
  * @return {Object}
  */
 
-module.exports = function SkipperS3(globalOpts) {
+module.exports = function SkipperAliyunOSS(globalOpts) {
   globalOpts = globalOpts || {};
 
-  var adapter = {
-    read: function (fd, cb) {
+  return {
+    read: function aliyunRead(fd, cb) {
       var prefix = fd;
       var store = oss({
         accessKeyId: globalOpts.key,
@@ -32,50 +32,53 @@ module.exports = function SkipperS3(globalOpts) {
         secure: true
       });
 
-      // Build a noop transform stream that will pump the S3 output through
-      var __transform__ = new Transform();
-      __transform__._transform = function (chunk, encoding, callback) {
+      // Build a noop transform stream that will pump the OSS output through
+      var transform = new Transform();
+      transform._transform = function(chunk, encoding, callback) {
         return callback(null, chunk);
       };
 
       co(store.getStream(prefix))
-        .then(function (getStreamResult) {
+        .then(function(getStreamResult) {
           // check whether we got an actual file stream:
           if (getStreamResult.res.status < 300) {
-            getStreamResult.stream.once('error', function (err) {
-              __transform__.emit('error', err);
+            getStreamResult.stream.once("error", function(err) {
+              transform.emit("error", err);
             });
-            getStreamResult.stream.pipe(__transform__);
+            getStreamResult.stream.pipe(transform);
           } else {
             var err = new Error();
             err.status = getStreamResult.res.status;
             err.headers = getStreamResult.res.headers;
-            err.message = 'Non-200 status code returned from Aliyun for requested file.';
-            __transform__.emit('error', err);
+            err.message =
+              "Non-200 status code returned from Aliyun for requested file.";
+            transform.emit("error", err);
           }
         })
-        .catch(function (err) {
-          __transform__.emit('error', err);
+        .catch(function(err) {
+          transform.emit("error", err);
         });
 
       if (cb) {
         var firedCb;
-        __transform__.once('error', function (err) {
+        transform.once("error", function(err) {
           if (firedCb) return;
           firedCb = true;
           cb(err);
         });
-        __transform__.pipe(concat(function (data) {
-          if (firedCb) return;
-          firedCb = true;
-          cb(null, data);
-        }));
+        transform.pipe(
+          concat(function(data) {
+            if (firedCb) return;
+            firedCb = true;
+            cb(null, data);
+          })
+        );
       }
 
-      return __transform__;
+      return transform;
     },
 
-    rm: function (fd, cb) {
+    rm: function aliyunRM(fd, cb) {
       var store = oss({
         accessKeyId: globalOpts.key,
         accessKeySecret: globalOpts.secret,
@@ -85,7 +88,7 @@ module.exports = function SkipperS3(globalOpts) {
       });
 
       co(store.delete(fd))
-        .then(function (result) {
+        .then(function(result) {
           if (result.status == 200) {
             cb();
           } else {
@@ -95,11 +98,11 @@ module.exports = function SkipperS3(globalOpts) {
             });
           }
         })
-        .catch(function (err) {
+        .catch(function(err) {
           cb(err);
         });
     },
-    ls: function (dirname, cb) {
+    ls: function aliyunLS(dirname, cb) {
       var store = oss({
         accessKeyId: globalOpts.key,
         accessKeySecret: globalOpts.secret,
@@ -109,82 +112,69 @@ module.exports = function SkipperS3(globalOpts) {
       });
 
       // Strip leading slash from dirname to form prefix
-      var prefix = dirname.replace(/^\//, '');
+      var prefix = dirname.replace(/^\//, "");
 
-      co(store.list({prefix: prefix}))
-        .then(function (result) {
+      co(store.list({ prefix: prefix }))
+        .then(function(result) {
           if (result && result.objects) {
-            cb(null, result.objects.map(function (object) {
-              return object.name;
-            }));
+            cb(
+              null,
+              result.objects.map(function(object) {
+                return object.name;
+              })
+            );
           } else {
             cb(null, []);
           }
         })
-        .catch(function (err) {
+        .catch(function(err) {
           cb(err, null);
         });
     },
 
-    receive: AliyunReceiver
-  };
+    receive: function aliyunReceiver(options) {
+      options = options || {};
+      options = _.defaults(options, globalOpts);
+      var headers = options.headers || {};
 
-  return adapter;
-
-  /**
-   * A simple receiver for Skipper that writes Upstreams to
-   * S3 to the configured bucket at the configured path.
-   *
-   * Includes a garbage-collection mechanism for failed
-   * uploads.
-   *
-   * @param  {Object} options
-   * @return {Stream.Writable}
-   */
-  function AliyunReceiver(options) {
-    options = options || {};
-    options = _.defaults(options, globalOpts);
-    var headers = options.headers || {};
-
-    var receiver__ = Writable({
-      objectMode: true
-    });
-
-    receiver__._write = function onFile(__newFile, encoding, next) {
-      var store = oss({
-        accessKeyId: options.key,
-        accessKeySecret: options.secret,
-        bucket: options.bucket,
-        region: globalOpts.region || undefined,
-        secure: true
+      var receiver = Writable({
+        objectMode: true
       });
 
-      var mimeType = headers['content-type'] || mime.lookup(__newFile.fd);
-
-      co(store.putStream(__newFile.fd, __newFile, {mime: mimeType}))
-        .then(function (result) {
-          __newFile.filename = __newFile.fd;
-          __newFile.fd = result.url;
-          return co(store.head(__newFile.filename));
-        })
-        .then(function (result) {
-          __newFile.extra = result.res.headers;
-          __newFile.byteCount = result.res.headers["content-length"];
-          receiver__.emit('writefile', __newFile);
-          next();
-        })
-        .catch(function (err) {
-          return next({
-            incoming: __newFile,
-            code: 'E_WRITE',
-            stack: typeof err === 'object' ? err.stack : new Error(err),
-            name: typeof err === 'object' ? err.name : err,
-            message: typeof err === 'object' ? err.message : err
-          });
+      receiver._write = function onFile(file, encoding, next) {
+        var store = oss({
+          accessKeyId: options.key,
+          accessKeySecret: options.secret,
+          bucket: options.bucket,
+          region: globalOpts.region || undefined,
+          secure: true
         });
 
-    };
+        var mimeType = headers["content-type"] || mime.getType(file.fd);
 
-    return receiver__;
-  }
+        co(store.putStream(file.fd, file, { mime: mimeType }))
+          .then(function(result) {
+            file.url = result.url;
+            return co(store.head(file.fd));
+          })
+          .then(function(result) {
+            file.extra = result.res.headers;
+            file.byteCount = result.res.headers["content-length"];
+            receiver.emit("writefile", file);
+            next();
+          })
+          .catch(function(err) {
+            return next({
+              incoming: file,
+              code: "E_WRITE",
+              stack: typeof err === "object" ? err.stack : new Error(err),
+              name: typeof err === "object" ? err.name : err,
+              message: typeof err === "object" ? err.message : err
+            });
+          });
+      };
+
+      return receiver;
+    }
+  };
 };
